aab64dbad0c861bccaf28e9b1eb4745e353aad65
zenhkm <zenhkm@gmail.com>
2026-08-26 21:59:07 +0700

    Remove untracked files and fix gitignore

--- patch2.py
+++
@@ -1,7 +0,0 @@
-with open('src/App.tsx', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-text = text.replace('              </div>\n  \n            {totalResults > 0 && (\n              <div className="flex justify-center', '              </div>\n            </div>\n  \n            {totalResults > 0 && (\n              <div className="flex justify-center')
-
-with open('src/App.tsx', 'w', encoding='utf-8') as f:
-    f.write(text)
