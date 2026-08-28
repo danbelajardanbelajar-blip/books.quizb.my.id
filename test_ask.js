fbde9cb87d45314872607caa7a6242574e71f647
zenhkm <zenhkm@gmail.com>
2026-08-28 10:44:22 +0700

    Remove .env again

--- test_ask.js
+++
@@ -1,8 +0,0 @@
-fetch('http://localhost:3000/api/ask', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ q: 'hantu' })
-})
-.then(res => res.text())
-.then(data => console.log(data))
-.catch(err => console.error(err));
