0554625414f1592bbf2e36237245f8dc066d4da1
zenhkm <zenhkm@gmail.com>
2026-08-28 10:40:17 +0700

    Remove accidentally committed local files

--- checkdb.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const dbPath = path.resolve('../golden_server/database.sqlite');
-console.log('Exists?', fs.existsSync(dbPath));
--- test_gemini.js
+++
@@ -1,9 +0,0 @@
-const apiKey = 'AIzaSyDDeLIfq3Vi4T3xwjQD9FJb_hRN2go7QpU';
-fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ contents: [{ parts: [{ text: "Hello" }] }] })
-})
-.then(res => res.json())
-.then(data => console.log(JSON.stringify(data)))
-.catch(err => console.error(err));
